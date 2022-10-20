554020a1af889cbd446cdfba6be4eb9908fd519d
kacarervin <kacarervin@gmail.com>
2022-10-20 09:24:20 +0200

    Finish crud

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
-import { addUser, deleteUser } from "./features/Users";
+import { addUser, deleteUser, updateUsername } from "./features/Users";
 import { useState } from "react";
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
   const userList = useSelector((state) => state.users.value);
   const [name, setName] = useState("");
   const [username, setUsername] = useState("");
+  const [newUsername, setNewUsername] = useState("");
 
   return (
     <div className="App">
@@ -42,15 +43,26 @@ function App() {
             <div>
               <h1>{user.name}</h1>
               <h1>{user.username}</h1>
-              <input type="text" placeholder="New username..." />
-              <button>Update Username</button>
+              <input
+                type="text"
+                placeholder="New username..."
+                onChange={(e) => setNewUsername(e.target.value)}
+              />
+              <button
+                onClick={() =>
+                  dispatch(
+                    updateUsername({ id: user.id, username: newUsername })
+                  )
+                }
+              >
+                Update Username
+              </button>
               <button onClick={() => dispatch(deleteUser({ id: user.id }))}>
                 Delete user
               </button>
             </div>
           );
         })}
-        {console.log(userList)}
       </div>
     </div>
   );

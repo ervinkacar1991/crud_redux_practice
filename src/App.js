17048e783a54d2c251ce6f3f9c033d65bfb1bacd
kacarervin <kacarervin@gmail.com>
2022-10-20 09:05:59 +0200

    Crud method with redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser, deleteUser } from "./features/Users";
+import { useState } from "react";
 
 function App() {
+  const dispatch = useDispatch();
+  const userList = useSelector((state) => state.users.value);
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="addUser">
+        <input
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Name..."
+        />
+        <input
+          onChange={(e) => setUsername(e.target.value)}
+          type="text"
+          placeholder="Username..."
+        />
+        <button
+          onClick={() => {
+            dispatch(
+              addUser({
+                id: userList[userList.length - 1].id + 1,
+                name,
+                username,
+              })
+            );
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add user
+        </button>
+      </div>
+      <div className="displayUsers">
+        {userList.map((user) => {
+          return (
+            <div>
+              <h1>{user.name}</h1>
+              <h1>{user.username}</h1>
+              <input type="text" placeholder="New username..." />
+              <button>Update Username</button>
+              <button onClick={() => dispatch(deleteUser({ id: user.id }))}>
+                Delete user
+              </button>
+            </div>
+          );
+        })}
+        {console.log(userList)}
+      </div>
     </div>
   );
 }
---
+++ src/FakeData.js
@@ -0,0 +1,42 @@
+export const UsersData = [
+  {
+    id: 1,
+    name: "Fare Mecinovic",
+    username: "Bret",
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+  },
+  {
+    id: 3,
+    name: "Shedule Plohovic",
+    username: "clementine",
+  },
+  {
+    id: 4,
+    name: "Fahrudin Pijus",
+    username: "Karianne",
+  },
+  {
+    id: 5,
+    name: "Dzenan Kosuta",
+    username: "Kamren",
+  },
+  {
+    id: 6,
+    name: "Remzo Gusinac",
+    username: "Leopoldo_Corkery",
+  },
+  {
+    id: 7,
+    name: "Mirza Delic",
+    username: "Karley_Dach",
+  },
+  {
+    id: 8,
+    name: "Dzemil Dupljak",
+    username: "Elwyn.Skiles",
+  },
+];
